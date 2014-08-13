b6b6d8459fcaf6f8513e30edf68f255f48445b96
Rick Kuipers <rick@thewebmen.com>
2014-08-13 13:21:25 +0200

    Fix bitnami download link

--- puppet/modules/bitnamistack/manifests/params.pp
+++ puppet/modules/bitnamistack/manifests/params.pp
@@ -1,7 +1,7 @@
 class bitnamistack::params {
 
     $install_file = "bitnami-lampstack-1.2-5-linux-installer.run"
-    $download_install_path = "http://bitnami.org/files/download/$bitnamistack::params::install_file"
+    $download_install_path = "http://bitnami.org/files/download/lamp/$bitnamistack::params::install_file"
     $stack_full_path = "/home/vagrant/lampstack"
 
 }
